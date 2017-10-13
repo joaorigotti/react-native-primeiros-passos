c32245e58b9e39cfc74e52fe7eccad4bf8171fa4
João Rigotti <joao.webdev@gmail.com>
2017-10-13 14:33:32 -0300

    Reorganizado o diretório __tests__

--- __tests__/Business/counter.js
+++
@@ -1,6 +0,0 @@
-import { increment, decrement } from '../../src/Business';
-
-test('increments 1 to 0', () => expect(increment(0)).toBe(1));
-test('limits max counter to 10', () => expect(increment(11)).toBe(10));
-test('decrements 1 to 10', () => expect(decrement(10)).toBe(9));
-test('limits minimum counter to 0', () => expect(decrement(0)).toBe(0));
